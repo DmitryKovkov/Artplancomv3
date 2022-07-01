84925575f88ab74d64a811b836c74d2e3c247c0d
DKovkov <d.a.kovkov@gmail.com>
2022-07-01 14:28:58 +0300

    modified finish

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,33 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Привет!");
-
-        // решаем задача по перевороту строк
+    public static void main(String[] args) throws IOException {
+        System.out.println("Привет! Введите строку:");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String str = reader.readLine();
+            System.out.println(str);
+            System.out.println(flipString(str));
+            repeatFlip(str,1000);
+            repeatFlip(str, 10000);
+            repeatFlip(str, 100000);
     }
+
+    public static String flipString(String str) {
+        if (str.length()==1) return str;
+        return flipString(str.substring(1)) + str.charAt(0);
+    }
+    public static void repeatFlip (String str, int n) {
+        long start = System.currentTimeMillis();
+            for (int i = 0; i < n; i++) {
+                str = flipString(str);
+            }
+        long finish = System.currentTimeMillis();
+        System.out.println(finish-start);
+    }
+
 }

180fe99108d2b006c0d919475d67d45d9ab0ec6c
DKovkov <d.a.kovkov@gmail.com>
2022-07-01 07:18:15 +0300

    add modified v1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Привет!");
-        // write your code here
+
+        // решаем задача по перевороту строк
     }
 }
